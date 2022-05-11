fa8582c1b7ba16a99aa4cf2a662ad0dede7b39a1
Jonathan <jonathan@electronic.mail>
2022-05-11 21:07:11 +0300

    remove comments in server/index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,7 +8,6 @@ let server: Server;
 
 const app = express();
 app.disable('x-powered-by');
-// app.use(compression); // npm compression
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(require('cookie-parser')());
@@ -16,27 +15,13 @@ app.use(require('cookie-parser')());
 routes(app);
 
 // error handler
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+app.use((err: any, req: express.Request, res: express.Response) => {
     console.error(err);
     return res.status(500).send();
 });
 
-// handle server close/restart
-// const closeServer = async () => {
-//     console.log('closing server and db');
-//     server && server.close((err) => console.log('server closed', {err}));
-//     // mongoClient && mongoClient.close(false);
-// }
-// ['SIGTERM', 'SIGINT'].forEach(sig => {
-//     process.on(sig, () => {
-//         console.log(sig + ' signal received');
-//         closeServer();
-//     });
-// });
-
 // connect to mongodb
 MongoClient.connect(process.env.MONGO_URI).then(client => {
-    // mongoClient = client;
     Collections.initialize(client.db(), client);
     console.log('connected to db');
     const port = process.env.PORT || 3002;
