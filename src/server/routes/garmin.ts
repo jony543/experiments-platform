be858869e71bf67aa30dfd29ce56b999db5637e3
SaarSagi <saarsagi@mail.tau.ac.il>
2022-07-29 11:21:20 +0300

    add functions in garmin.ts

--- src/server/routes/garmin.ts
+++ src/server/routes/garmin.ts
@@ -1,10 +1,39 @@
 import express from 'express';
+import { getCollection } from '../services/collections';    // function for access mongodb specific collection
 
 const garminRouter = express.Router();
+const garminCollection ="garmin-experiments-db";
+const success = "success";
+const err = "error";
 
-// example
-garminRouter.get('/activities', (req, res) => {
-    return res.status(200).send('result!');
+// store the content of a new request in matching mongo collection
+function updateDBCollection(collectionName:string, req){
+    var collection = getCollection(garminCollection);       // access subcollection with collectionName
+    var rawData = req.body;                                 // the content of the request
+    var newDocuments = rawData[collectionName];             // array of documents sent from garmin 
+    
+    newDocuments.forEach((doc) => {                         // for each document in the array
+        doc.type = collectionName;                          // add a new key named "type" with value collectionName
+        // define a new id field and insert the document if not exists 
+        collection.updateOne({"new_id":doc["summaryId"]}, {$setOnInsert: doc}, {upsert:true});
+    });
+    // print message
+    console.log(`\n ~~~~~${collectionName} DATA SENT TO MONGO~~~~\n`);
+}
+
+// called by garmin server, process the request, '/test' is the relative path
+garminRouter.post('/garmindata', async (req, res) => {
+    // req.body is an object with one key (collectionName) and value (array of documents)
+    const collectionName = Object.keys(req.body)[0];
+
+    if (req.method == 'POST' &&
+        req.headers['user-agent'] == 'Garmin Health API'){      // handle only post request from garmin
+        updateDBCollection(collectionName, req);                // store data in mongodb
+        res.send(success);                                    // return a promise, garmin expect 200 to be returned
+    }
+    else{
+        res.send(err);
+    }
 });
 
 export default garminRouter;
--- src/server/services/collections.ts
+++ src/server/services/collections.ts
@@ -1,6 +1,6 @@
 import { omit } from "lodash";
-import { Db, Filter, MongoClient, ObjectId, OptionalUnlessRequiredId } from "mongodb";
-import { BaseModel, Experiment, Session, User, Worker } from "../types/models";
+import { Db, Filter, MongoClient, ObjectId, OptionalId, OptionalUnlessRequiredId, WithId } from "mongodb";
+import { BaseModel, Experiment, User } from "../types/models";
 import { objectId } from "../utils/models";
 
 let client: MongoClient;
@@ -10,35 +10,23 @@ export const initializeCollections = (_db: Db, _client: MongoClient) => {
     db = _db;
     client = _client;
 }
-export type CollectionName = 'users' | 'workers' | 'sessions' | 'experiments';
+export type CollectionName = 'users' | 'workers' | 'sessions' | 'experiments' | 'garmin-experiments-db';
 export const getCollection = <T>(name: CollectionName) => db.collection<T>(name);
-type CollectionModel<T extends CollectionName> =
-    T extends 'sessions' ? Session :
+type CollectionModel<T extends CollectionName> = OptionalId<
     T extends 'users' ? User : 
     T extends 'experiments' ? Experiment :
-    T extends 'workers' ? Worker :
-    BaseModel;
+    BaseModel
+>;
 
 export const find = <T extends CollectionName>(collectionName: T, filter: Filter<CollectionModel<T>>) =>
     getCollection<CollectionModel<T>>(collectionName).find(filter).toArray();
-    
 export const findOne = <T extends CollectionName>(collectionName: T, filter: Filter<CollectionModel<T>>) =>
     getCollection<CollectionModel<T>>(collectionName).findOne(filter);
-
 export const get = <T extends CollectionName>(collectionName: T, id: string | ObjectId) => 
     findOne(collectionName, { _id: objectId(id) as any });
-
-// TODO - createdAt
 export const insertOne = async <T extends CollectionName>(collectionName: T, item: OptionalUnlessRequiredId<CollectionModel<T>>) => {
     const {insertedId} = await getCollection<CollectionModel<T>>(collectionName).insertOne(item);
     return await get(collectionName, insertedId);
-};
-
-// TODO - updatedAt
+}
 export const updateOne = async <T extends CollectionName>(collectionName: T, id: string | ObjectId, update: Partial<CollectionModel<T>>) =>
-    getCollection<CollectionModel<T>>(collectionName).updateOne(
-        {_id: objectId(id) as any}, 
-        {$set: omit(update, '_id') as Partial<CollectionModel<T>>});
-
-export const deleteOne = <T extends CollectionName>(collectionName: T, id: string | ObjectId) => 
-    getCollection(collectionName).deleteOne({_id: objectId(id)});
+    getCollection<CollectionModel<T>>(collectionName).updateOne({_id: objectId(id) as any}, {$set: omit(update, '_id') as Partial<CollectionModel<T>>})
